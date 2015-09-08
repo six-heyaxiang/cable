3468863d4123e754c1d0f6e03505ebda590e7152
heyaxiang <six.heyaxiang@gmail.com>
2015-09-08 09:55:30 +0800

    coding

---
+++ src/com/cable/bean/result/ResultOrbitInfo.java
@@ -0,0 +1,18 @@
+package com.cable.bean.result;
+
+import java.util.List;
+
+import com.cable.bean.Orbit;
+
+
+public class ResultOrbitInfo {
+
+	private List<Orbit> orbits;
+
+	public List<Orbit> getOrbits() {
+		return orbits;
+	}
+	public void setOrbits(List<Orbit> orbits) {
+		this.orbits = orbits;
+	}
+}
